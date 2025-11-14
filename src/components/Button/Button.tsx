b23a68ea2a77cc1098f4fc4ef9ed1891f604839f
Yurii-Diner <yuriidiner@gmail.com>
2025-11-14 16:33:29 +0100

    add hw_8

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import { ButtonComponent } from "./styles";
+import type { ButtonProps } from "./types";
+
+
+function Button({
+  type = "button",
+  name,
+  onClick,
+  disabled = false,
+  isDanger
+}: ButtonProps) {
+  return (
+    <ButtonComponent
+      className="button"
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      isDanger={isDanger}
+    >
+      {name}
+    </ButtonComponent>
+  );
+}
+
+export default Button;
