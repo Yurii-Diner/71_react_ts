6f772acbd1aee9e9322c8d58f0e66a29b37b644a
Yurii-Diner <yuriidiner@gmail.com>
2025-11-24 13:11:44 +0100

    add hw_14

---
+++ src/homeworks/Homework14/Homework14.tsx
@@ -0,0 +1,13 @@
+
+import BlogManagement from './components/Blogmanagement/BlogManagement';
+import { Homework14Container } from './styles'
+
+function Homework14() {
+    return (
+        <Homework14Container>
+            <BlogManagement />
+        </Homework14Container>
+    )
+}
+
+export default Homework14;
